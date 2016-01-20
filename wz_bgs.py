44adba0153ea36275e7d102fded60a873c776db1
Erik Hvatum <ice.rikh@gmail.com>
2016-01-19 18:50:16 -0600

    Implement computeFocusMeasures.

--- wz_bgs.py
+++ wz_bgs.py
@@ -25,6 +25,7 @@
 import freeimage
 import math
 import numpy
+from pathlib import Path
 import time
 from zplib.image import mask as zplib_image_mask
 
@@ -127,10 +128,130 @@ def processFlipbookPages(pages, temporal_radius=11):
         pass
     return ret, wzBgs
 
-def computeFocusMeasures():
-    from rpc_acquisition.scope.device.autofocus import MultiBrenner
+from rpc_acquisition.scope.device.autofocus import MultiBrenner
+import os.path
+import wautofocuser
+from zplib.image import fast_fft
+
+FFTW_WISDOM = os.path.expanduser('~/fftw_wisdom')
+if os.path.exists(FFTW_WISDOM):
+    fast_fft.load_plan_hints(FFTW_WISDOM)
+
+class MaskedAutofocusMetric:
+    def __init__(self, shape):
+        self.focus_scores = []
+
+    def evaluate_image(self, image, mask):
+        self.focus_scores.append(self.metric(image, mask))
+
+    def metric(self, image, mask):
+        raise NotImplementedError()
+
+    def find_best_focus_index(self):
+        best_i = numpy.argmax(self.focus_scores)
+        focus_scores = self.focus_scores
+        self.focus_scores = []
+        return best_i, focus_scores
+
+class MaskedBrenner(MaskedAutofocusMetric):
+    def metric(self, image, mask):
+        image = image.astype(numpy.float32) # otherwise can get overflow in the squaring and summation
+        # Exclude unmasked regions from edge detection output lest the masked region's border register as a large, spurious edge that may
+        # dominate the measure and make it a measure of mask region border length rather than information density within the masked region
+        # (depending on image content and spatial filter preprocessing).
+        x_diffs = (image[2:, :] - image[:-2, :])**2
+        x_diffs[mask[:2558,:]] = 0
+        y_diffs = (image[:, 2:] - image[:, :-2])**2
+        y_diffs[mask[:,:2158]] = 0
+        return x_diffs.sum() + y_diffs.sum()
+
+class MaskedFilteredBrenner(MaskedBrenner):
+    def __init__(self, shape):
+        super().__init__(shape)
+        t0 = time.time()
+        self.filter = fast_fft.SpatialFilter(shape, self.PERIOD_RANGE, precision=32, threads=64, better_plan=False)
+        if time.time() - t0 > 0.5:
+            fast_fft.store_plan_hints(FFTW_WISDOM)
+
+    def metric(self, image, measure_mask):
+        filtered = self.filter.filter(image)
+        return super().metric(filtered, measure_mask)
+
+class MaskedHighpassBrenner(MaskedFilteredBrenner):
+    PERIOD_RANGE = (None, 10)
+
+class MaskedBandpassBrenner(MaskedFilteredBrenner):
+    PERIOD_RANGE = (60, 100)
+
+class MaskedMultiBrenner(MaskedAutofocusMetric):
+    def __init__(self, shape):
+        super().__init__(shape)
+        self.hp = MaskedHighpassBrenner(shape)
+        self.bp = MaskedBandpassBrenner(shape)
+
+    def metric(self, image, measure_mask):
+        return self.hp.metric(image, measure_mask), self.bp.metric(image, measure_mask)
+
+DELTAS = []
+MASKS = []
+
+def _computeFocusMeasures(bgs, im_fpath, measure_mask):
+    try:
+        im = freeimage.read(str(im_fpath))
+    except:
+        return
+    if bgs.model is not None:
+        try:
+            delta = numpy.abs(bgs.queryModelDelta(im))
+            mask = bgs.queryModelMask(im, delta)
+            DELTAS.append(delta)
+            MASKS.append(mask)
+        except:
+            return
+        focus_measures = {}
+        focus_measures['whole_image_hp_brenner_sum_of_squares'], focus_measures['whole_image_bp_brenner_sum_of_squares'] = MultiBrenner((2560, 2160)).metric(im)
+        model_delta_squares = delta.astype(numpy.float64)**2
+        focus_measures['model_delta_sum_of_squares'] = model_delta_squares.sum()
+        focus_measures['model_mask_count'] = mask.sum()
+        model_delta_squares[measure_mask] = 0
+        focus_measures['model_mask_region_delta_sum_of_squares'] = model_delta_squares.sum()
+        focus_measures['model_mask_region_image_hp_brenner_sum_of_squares'], focus_measures['model_mask_region_image_bp_brenner_sum_of_squares'] = MaskedMultiBrenner((2560,2160)).metric(im, measure_mask)
+        return focus_measures
+
+def computeFocusMeasures(temporal_radius=11):
     import sqlite3
-    db = sqlite3.connect('/mnt/bulkdata/Sinha_Drew/2015.11.13_ZPL8Prelim3/analysis/db.sqlite3')
+    with sqlite3.connect('/mnt/bulkdata/Sinha_Drew/2015.11.13_ZPL8Prelim3/analysis/db.sqlite3') as db:
+        db.row_factory = sqlite3.Row
+        non_vignette = freeimage.read('/mnt/bulkdata/Sinha_Drew/2015.11.13_ZPL8Prelim3/non-vignette.png')
+        vignette = non_vignette == 0
+        positions = [row['well_idx'] for row in db.execute('select well_idx from wells where did_hatch')]
+        position_bgss = {position : WzBgs(2560, 2160, temporal_radius, non_vignette) for position in positions}
+        time_points = [row['name'] for row in db.execute('select name from time_points')]
+        for time_point in time_points:
+            if time_point == '2015-11-13t1848':
+                return
+            print(time_point)
+            for position in positions:
+                print('', position)
+                acquisition_names = [row['acquisition_name'] for row in db.execute('select * from images where well_idx=? and time_point=?', (position, time_point))]
+                if acquisition_names:
+                    bgs = position_bgss[position]
+                    for acquisition_name in acquisition_names:
+                        print(' ', acquisition_name)
+                        im_fpath = Path('/mnt/bulkdata/Sinha_Drew/2015.11.13_ZPL8Prelim3/') / '{:02}'.format(position) / '{} {}_ffc.png'.format(time_point, acquisition_name)
+                        focus_measures = _computeFocusMeasures(bgs, im_fpath, vignette)
+                        if focus_measures is not None:
+                            measure_names = sorted(focus_measures.keys())
+                            q = 'update images set ' + ', '.join('{}=?'.format(measure_name) for measure_name in measure_names)
+                            q+= ' where well_idx=? and time_point=? and acquisition_name=?'
+                            v = [float(focus_measures[measure_name]) for measure_name in measure_names]
+                            v.extend((position, time_point, acquisition_name))
+                            list(db.execute(q, v))
+                    try:
+                        bgs.updateModel(freeimage.read(str(Path('/mnt/bulkdata/Sinha_Drew/2015.11.13_ZPL8Prelim3/') / '{:02}'.format(position) / '{} bf_ffc.png'.format(time_point))))
+                    except:
+                        pass
+                    db.commit()
 
 if __name__ == '__main__':
     import sys
